ca9adfe20797413117ae2113363bbb271a4d15a3
mufflon12 <mufflon12@googlemail.com>
2021-05-30 22:01:08 +0200

    Classic Rotation Beta Release

---
+++ Rotations/classic/Paladin/Classic Protection Paladin.cs
@@ -0,0 +1,224 @@
+﻿using System.Diagnostics;
+
+namespace HyperElk.Core
+{
+    public class ClassicTankWarrior : CombatRoutine
+    {
+        private string Startattack = "Startattack";
+        private string RighteousFury = "Righteous Fury";
+        private string HammerofWrath = "Hammer of Wrath";
+        private string Judgement = "Judgement";
+        private string AvengesShield = "Avenger's Shield";
+        private string Consecration = "Consecration";
+        private string HolyShield = "Holy Shield";
+        private string AvengingWrath = "Avenging Wrath";
+        private string SealoftheCrusader = "Seal of the Crusader";
+        private string SealofVengeance = "Seal of Vengeance";
+        private string SealofWisdom = "Seal of Wisdom";
+        private string SealofRighteousness = "Seal of Righteousness";
+        private string DivineShield = "Divine Shield";
+        private string Auras = "Auras";
+        private string DevotionAura = "Devotion Aura";
+        private string RetributionAura = "Retribution Aura";
+        private string SanctityAura = "Sanctity Aura";
+        private string CrusaderAura = "Crusader Aura";
+        private bool UseAutoAura => CombatRoutine.GetPropertyBool("AutoAuraSwitch");
+
+        private bool UseSealoftheCrusader => CombatRoutine.GetPropertyBool(SealoftheCrusader);
+        private bool UseSealofVengeance => CombatRoutine.GetPropertyBool(SealofVengeance);
+        private bool UseSealofWisdom => CombatRoutine.GetPropertyBool(SealofWisdom);
+        private int SealofWisdomManaProc => CombatRoutine.GetPropertyInt("SealofWisdomMana");
+        private bool UseCrusaderAura => CombatRoutine.GetPropertyBool(CrusaderAura);
+
+        private int DivineShieldHealthProc => CombatRoutine.GetPropertyInt(DivineShield);
+        string[] AuraList = new string[] { "None", "Devotion Aura", "Retribution Aura" };
+
+        private string UseAura => AuraList[CombatRoutine.GetPropertyInt(Auras)];
+        bool IsDevotionAura => UseAura == "Devotion Aura";
+        bool IsRetributionAura => UseAura == "Retribution Aura";
+
+        public override void Initialize()
+        {
+            CombatRoutine.Name = "Classic Protection Paladin";
+            CombatRoutine.TBCRotation = true;
+            API.WriteLog("Classic Protection Paladin by Mufflon12");
+
+            CombatRoutine.AddSpell(RighteousFury);
+            CombatRoutine.AddSpell(HammerofWrath);
+            CombatRoutine.AddSpell(Judgement);
+            CombatRoutine.AddSpell(AvengesShield);
+            CombatRoutine.AddSpell(Consecration);
+            CombatRoutine.AddSpell(HolyShield);
+            CombatRoutine.AddSpell(AvengingWrath);
+            CombatRoutine.AddSpell(SealoftheCrusader);
+            CombatRoutine.AddSpell(SealofVengeance);
+            CombatRoutine.AddSpell(SealofRighteousness);
+            CombatRoutine.AddSpell(DivineShield);
+          
+            CombatRoutine.AddSpell(CrusaderAura);     
+            CombatRoutine.AddSpell(DevotionAura);
+            CombatRoutine.AddSpell(RetributionAura);
+
+
+            CombatRoutine.AddDebuff(SealofRighteousness);
+            CombatRoutine.AddDebuff(SealofVengeance);
+            CombatRoutine.AddDebuff(SealoftheCrusader);
+            CombatRoutine.AddDebuff(Consecration);
+
+            CombatRoutine.AddBuff(HolyShield);
+            CombatRoutine.AddDebuff(Judgement);
+            CombatRoutine.AddBuff(RighteousFury);
+
+
+
+
+            CombatRoutine.AddMacro(Startattack);
+
+            CombatRoutine.AddProp(SealoftheCrusader, SealoftheCrusader, false, "Enable if you want to use " + SealoftheCrusader, "Class Specific");
+            CombatRoutine.AddProp(SealofVengeance, SealofVengeance, false, "Enable if you want to use " + SealofVengeance, "Class Specific");
+
+            CombatRoutine.AddProp(SealofWisdom, SealofWisdom, false, "Enable if you want to use " + SealofWisdom, "Seal of Wisdom");
+            CombatRoutine.AddProp(SealofWisdom, SealofWisdom + "Mana %", 50, "Seal of Wisdom below XX Mana % ", "Seal of Wisdom");
+
+            CombatRoutine.AddProp(DivineShield, DivineShield + "Health %", 50, "Health % to use " + DivineShield, "Healing");
+
+            CombatRoutine.AddProp(Auras, "Wich Aura", AuraList, "Chose your Aura", "Aura", 0);
+            CombatRoutine.AddProp(CrusaderAura, CrusaderAura, false, "Enable if you want to switch to " + CrusaderAura + " When Mounted", "Auras");
+            CombatRoutine.AddProp("AutoAuraSwitch", "Auto Aura Switch", false, "Enable if you want to Auto Switch Auras", "Auras");
+
+        }
+        public override void Pulse()
+        {
+            if (API.CanCast(CrusaderAura) && UseAutoAura && UseCrusaderAura && API.Classic_PlayerShapeShiftForm !=7 && API.PlayerIsMounted)
+            {
+                API.CastSpell(CrusaderAura);
+                return;
+            }
+            if (API.CanCast(DevotionAura) && UseAutoAura && API.Classic_PlayerShapeShiftForm != 1 && !API.PlayerIsMounted && IsDevotionAura)
+            {
+                API.CastSpell(DevotionAura);
+                return;
+            }
+            if (API.CanCast(RetributionAura) && UseAutoAura && API.Classic_PlayerShapeShiftForm != 2 && !API.PlayerIsMounted && IsRetributionAura)
+            {
+                API.CastSpell(RetributionAura);
+                return;
+            }
+
+        }
+        public override void CombatPulse()
+        {
+            if (API.CanCast(DivineShield) && API.PlayerHealthPercent <= DivineShieldHealthProc)
+            {
+                API.CastSpell(DivineShield);
+                return;
+            }
+            if (API.CanCast(RighteousFury) && !API.PlayerHasBuff(RighteousFury))
+            {
+                API.CastSpell(RighteousFury);
+                return;
+            }
+            if (IsCooldowns && API.CanCast(AvengingWrath))
+            {
+                API.CastSpell(AvengingWrath);
+                return;
+            }
+            if (API.CanCast(HammerofWrath) && API.TargetHealthPercent <= 20)
+            {
+                API.CastSpell(HammerofWrath);
+                return;
+            }
+            if (IsAOE && API.PlayerUnitInMeleeRangeCount >= AOEUnitNumber)
+            {
+                if (API.CanCast(SealofRighteousness) && API.PlayerTimeInCombat < 500 && !API.TargetHasDebuff(SealofRighteousness))
+                {
+                    API.CastSpell(SealofRighteousness);
+                    return;
+                }
+                if (API.CanCast(SealofWisdom) && !API.TargetHasDebuff(SealofWisdom) && UseSealofWisdom && API.PlayerMana < SealofWisdomManaProc)
+                {
+                    API.CastSpell(SealofWisdom);
+                    return;
+                }
+                if (API.CanCast(SealofVengeance) && !API.TargetHasDebuff(SealofVengeance) && UseSealofVengeance && API.PlayerMana > SealofWisdomManaProc)
+                {
+                    API.CastSpell(SealofVengeance);
+                    return;
+                }
+                if (API.CanCast(SealoftheCrusader) && !API.TargetHasDebuff(SealoftheCrusader) && UseSealoftheCrusader && API.PlayerMana > SealofWisdomManaProc)
+                {
+                    API.CastSpell(SealoftheCrusader);
+                    return;
+                }
+                if (API.CanCast(HolyShield) && !API.PlayerHasBuff(HolyShield))
+                {
+                    API.CastSpell(HolyShield);
+                    return;
+                }
+                if (API.CanCast(Consecration) && !API.TargetHasDebuff(Consecration))
+                {
+                    API.CastSpell(Consecration);
+                    return;
+                }
+                if (API.CanCast(AvengesShield))
+                {
+                    API.CastSpell(AvengesShield);
+                    return;
+                }
+                if (API.CanCast(Judgement) && API.TargetDebuffRemainingTime(Judgement) < 100)
+                {
+                    API.CastSpell(Judgement);
+                    return;
+                }
+            }
+            if (IsAOE && API.PlayerUnitInMeleeRangeCount <= AOEUnitNumber || !IsAOE)
+            {
+                if (API.CanCast(SealofRighteousness) && API.PlayerTimeInCombat < 500 && !API.TargetHasDebuff(SealofRighteousness))
+                {
+                    API.CastSpell(SealofRighteousness);
+                    return;
+                }
+                if (API.CanCast(SealofWisdom) && !API.TargetHasDebuff(SealofWisdom) && UseSealofWisdom && API.PlayerMana < SealofWisdomManaProc)
+                {
+                    API.CastSpell(SealofWisdom);
+                    return;
+                }
+                if (API.CanCast(SealofVengeance) && !API.TargetHasDebuff(SealofVengeance) && UseSealofVengeance && API.PlayerMana > SealofWisdomManaProc)
+                {
+                    API.CastSpell(SealofVengeance);
+                    return;
+                }
+                if (API.CanCast(SealoftheCrusader) && !API.TargetHasDebuff(SealoftheCrusader) && UseSealoftheCrusader && API.PlayerMana > SealofWisdomManaProc)
+                {
+                    API.CastSpell(SealoftheCrusader);
+                    return;
+                }
+                if (API.CanCast(HolyShield) && !API.PlayerHasBuff(HolyShield))
+                {
+                    API.CastSpell(HolyShield);
+                    return;
+                }
+                if (API.CanCast(Consecration) && !API.TargetHasDebuff(Consecration))
+                {
+                    API.CastSpell(Consecration);
+                    return;
+                }
+                if (API.CanCast(AvengesShield))
+                {
+                    API.CastSpell(AvengesShield);
+                    return;
+                }
+                if (API.CanCast(Judgement) && API.TargetDebuffRemainingTime(Judgement) < 100)
+                {
+                    API.CastSpell(Judgement);
+                    return;
+                }
+            }
+
+        }
+        public override void OutOfCombatPulse()
+        {
+
+        }
+    }
+}
---
+++ Rotations/classic/Rogue/Classic Rogue.cs
@@ -0,0 +1,160 @@
+﻿using System.Diagnostics;
+
+namespace HyperElk.Core
+{
+    public class ClassicWarrior : CombatRoutine
+    {
+        private string SinisterStrike = "Sinister Strike";
+        private string SliceandDice = "Slice and Dice";
+        private string Eviscerate = "Eviscerate";
+        private string Gouge = "Gouge";
+        private string Startattack = "Startattack";
+        private string Evasion = "Evasion";
+        private string ExposeArmor = "Expose Armor";
+        private string Rupture = "Rupture";
+        private string Kick = "Kick";
+        private string BladeFlurry = "Blade Flurry";
+        int[] numbList = new int[] { 0, 1, 2, 3, 4, 5 };
+        private int SliceandDiceStackCount => numbList[CombatRoutine.GetPropertyInt(SliceandDice)];
+        private int EviscerateStackCount => numbList[CombatRoutine.GetPropertyInt(Eviscerate)];
+        private int ExposeArmorStackCount => numbList[CombatRoutine.GetPropertyInt(ExposeArmor)];
+        private int RuptureStackCount => numbList[CombatRoutine.GetPropertyInt(Rupture)];
+
+        private int EvasionHealthPercent => CombatRoutine.GetPropertyInt(Evasion);
+
+        private bool UseGouge => CombatRoutine.GetPropertyBool(Gouge);
+        private bool UseExposeArmor => CombatRoutine.GetPropertyBool("UseExposeArmor");
+
+
+        public override void Initialize()
+        {
+            CombatRoutine.Name = "Classic Rogue";
+            CombatRoutine.TBCRotation = true;
+            API.WriteLog("Classic Rogue by Mufflon12");
+
+            CombatRoutine.AddSpell(SliceandDice);
+            CombatRoutine.AddSpell(Eviscerate);
+            CombatRoutine.AddSpell(SinisterStrike);
+            CombatRoutine.AddSpell(Gouge);
+            CombatRoutine.AddSpell(ExposeArmor);
+            CombatRoutine.AddSpell(Rupture);
+            CombatRoutine.AddSpell(Kick);
+            CombatRoutine.AddSpell(BladeFlurry);
+            CombatRoutine.AddSpell(Evasion);
+
+            CombatRoutine.AddBuff(Rupture);
+            CombatRoutine.AddBuff(SliceandDice);
+            CombatRoutine.AddBuff(Gouge);
+            CombatRoutine.AddBuff(Eviscerate);
+
+            CombatRoutine.AddMacro(Startattack);
+
+
+            CombatRoutine.AddProp(SliceandDice, SliceandDice, numbList, " How many many Combo Points to use Slice and Dice", "Class Specific", 5);
+            CombatRoutine.AddProp(Eviscerate, Eviscerate, numbList, " How many Combo Points to use Eviscerate", "Class Specific", 5);
+
+            CombatRoutine.AddProp(ExposeArmor, ExposeArmor, numbList, " How many many Combo Points to use Expose Armor", "Class Specific", 5);
+            CombatRoutine.AddProp("UseExposeArmor", "Expose Armor", true, "Enable if you want to use Expose Armor", "Class Specific");
+
+            CombatRoutine.AddProp(Rupture, Rupture, numbList, " How many many Combo Points to use Rupture", "Class Specific", 2);
+
+            CombatRoutine.AddProp(Gouge, Gouge, true, "Enable if you want to use Gouge", "Class Specific");
+            CombatRoutine.AddProp(Evasion, Evasion, 60, "Life Percent to use Evasion", "Class Specific");
+
+        }
+        public override void Pulse()
+        {
+
+        }
+        public override void CombatPulse()
+        {
+            if (API.CanCast(Evasion) && API.PlayerHealthPercent <= EvasionHealthPercent)
+            {
+                API.CastSpell(Evasion);
+                return;
+            }
+            if (isInterrupt && API.CanCast(Kick))
+            {
+                API.CastSpell(Kick);
+                return;
+            }
+            if (!API.PlayerIsAutoAttack)
+            {
+                API.CastSpell(Startattack);
+                return;
+            }
+            if (IsCooldowns && API.CanCast(BladeFlurry))
+            {
+                API.CastSpell(BladeFlurry);
+                return;
+            }
+            if (IsAOE && API.PlayerUnitInMeleeRangeCount >= AOEUnitNumber && API.TargetRange <= 5)
+            {
+
+                if (API.CanCast(SliceandDice) && API.PlayerComboPoints >= SliceandDiceStackCount && !API.PlayerHasBuff(SliceandDice))
+                {
+                    API.CastSpell(SliceandDice);
+                    return;
+                }
+                if (API.CanCast(Rupture) && API.PlayerComboPoints >= RuptureStackCount && !API.TargetHasDebuff(Rupture))
+                {
+                    API.CastSpell(Rupture);
+                    return;
+                }
+                if (API.CanCast(Eviscerate) && API.PlayerComboPoints >= EviscerateStackCount && API.PlayerHasBuff(SliceandDice))
+                {
+                    API.CastSpell(Eviscerate);
+                    return;
+                }
+                if (API.CanCast(Gouge) && UseGouge)
+                {
+                    API.CastSpell(Gouge);
+                    return;
+                }
+                if (API.CanCast(ExposeArmor) && UseExposeArmor && API.PlayerComboPoints >= ExposeArmorStackCount)
+                {
+                    API.CanCast(ExposeArmor);
+                    return;
+                }
+                if (API.CanCast(SinisterStrike))
+                {
+                    API.CastSpell(SinisterStrike);
+                    return;
+                }
+            }
+            if ((IsAOE && API.PlayerUnitInMeleeRangeCount <= AOEUnitNumber || !IsAOE) && API.TargetRange <= 5)
+            {
+                if (API.CanCast(SliceandDice) && API.PlayerComboPoints >= SliceandDiceStackCount && !API.PlayerHasBuff(SliceandDice))
+                {
+                    API.CastSpell(SliceandDice);
+                    return;
+                }
+                if (API.CanCast(Eviscerate) && API.PlayerComboPoints >= EviscerateStackCount && API.PlayerHasBuff(SliceandDice))
+                {
+                    API.CastSpell(Eviscerate);
+                    return;
+                }
+                if (API.CanCast(Gouge) && UseGouge)
+                {
+                    API.CastSpell(Gouge);
+                    return;
+                }
+                if (API.CanCast(ExposeArmor) && UseExposeArmor && API.PlayerComboPoints >= ExposeArmorStackCount)
+                {
+                    API.CanCast(ExposeArmor);
+                    return;
+                }
+                if (API.CanCast(SinisterStrike))
+                {
+                    API.CastSpell(SinisterStrike);
+                    return;
+                }
+            }
+
+        }
+        public override void OutOfCombatPulse()
+        {
+
+        }
+    }
+}
